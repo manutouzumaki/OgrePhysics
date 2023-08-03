04f8ac48f4123b74ee20f165a7f97ec04f23e9b7
manuel <manuelcabrerizo5@gmail.com>
2023-08-03 13:01:29 -0300

    first commit

---
+++ src/build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if not exist ..\build mkdir ..\build
+
+set CFLAGS=/nologo /Od /Zi /EHsc
+set LIBS=OgreBites.lib OgreRTShaderSystem.lib OgreMain.lib
+set INC_DIR=/I"D:\Libs\ogre\sdk\include\OGRE" /I"D:\Libs\ogre\sdk\include\OGRE\Bites" /I"D:\Libs\ogre\sdk\include\OGRE\RTShaderSystem"
+set LNK_DIR=/LIBPATH:"D:\Libs\ogre\sdk\lib"
+
+pushd ..\build
+
+    cl %CFLAGS% %INC_DIR% ..\src\main.cpp /Fe.\game /link %LNK_DIR% %LIBS%
+
+popd
